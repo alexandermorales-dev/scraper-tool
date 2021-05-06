1f31c0beb81b299bd26c664c857a52d2629bb02d
Alexander Morales <alexandermorales.n1@gmail.com>
2021-05-05 20:22:00 -0500

    fix linter errors

--- scraper.rb
+++ scraper.rb
@@ -1,18 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = Nokogiri::HTML(URI.open("https://www.encuentra24.com/panama-es/searchresult/bienes-raices-venta-de-propiedades?q=f_price.-false&regionslug=prov-panama"))
+html = Nokogiri::HTML(URI.open('https://www.encuentra24.com/panama-es/searchresult/bienes-raices-venta-de-propiedades?q=f_price.-false&regionslug=prov-panama'))
 
-articles = html.css("article")
+articles = html.css('article')
 
-titles = articles.map { |x| x.css('.ann-box-title').text}
-prices = articles.map { |x| x.css('.ann-price-2nd').text}
-
-master_arr = [titles, prices]
+titles = articles.map { |x| x.css('.ann-box-title').text }
+prices = articles.map { |x| x.css('.ann-price-2nd').text }
 
 i = 0
 
 while i < titles.length
-  puts "Property name:\n#{titles[i]} \nprice: #{prices[i]}" 
+  puts "Property name:\n#{titles[i]} \nprice: #{prices[i]}"
   i += 1
 end
