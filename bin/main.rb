b35aa3537e5d73ea205064fc4c0d937f4a93b960
Alexander Morales <alexandermorales.n1@gmail.com>
2021-05-06 14:16:20 -0500

    fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -2,32 +2,31 @@ require '../lib/methods'
 require 'nokogiri'
 require 'open-uri'
 
-system "cls"
-system "clear"
+system 'cls'
+system 'clear'
 puts 'Insert the URL you want to scrape: '
 url = gets.chomp
 sleep(1)
-system "cls"
-system "clear"
+system 'cls'
+system 'clear'
 puts 'Insert the name of the container class holding the elements you are interested in: '
 container = gets.chomp
-system "cls"
-system "clear"
+system 'cls'
+system 'clear'
 puts 'Insert the name of the title class: '
 titles = gets.chomp
-system "cls"
-system "clear"
+system 'cls'
+system 'clear'
 puts 'Insert the name of the prices class: '
 prices = gets.chomp
 
 html = Link.new(url)
-html.listings("#{container}")
-html.titles("#{titles}")
-html.prices("#{prices}")
+html.listings(container.to_s)
+html.titles(titles.to_s)
+html.prices(prices.to_s)
 
 html.all
 # str = "Property name: #{@titles[i]} \n Price: #{@prices[i]}"
-
 
 # https://www.encuentra24.com/panama-es/searchresult/bienes-raices-venta-de-propiedades?regionslug=prov-panama&q=f_price.-false
 
