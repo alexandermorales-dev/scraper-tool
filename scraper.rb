d15811eadf9b51da7c85a137fc2c900b71747316
Alexander Morales <alexandermorales.n1@gmail.com>
2021-05-05 20:18:38 -0500

    implement nokogiri logic

---
+++ scraper.rb
@@ -0,0 +1,18 @@
+require 'nokogiri'
+require 'open-uri'
+
+html = Nokogiri::HTML(URI.open("https://www.encuentra24.com/panama-es/searchresult/bienes-raices-venta-de-propiedades?q=f_price.-false&regionslug=prov-panama"))
+
+articles = html.css("article")
+
+titles = articles.map { |x| x.css('.ann-box-title').text}
+prices = articles.map { |x| x.css('.ann-price-2nd').text}
+
+master_arr = [titles, prices]
+
+i = 0
+
+while i < titles.length
+  puts "Property name:\n#{titles[i]} \nprice: #{prices[i]}" 
+  i += 1
+end
