6cf8fff6563ee47550b8033687e32046d083a69c
Alexander Morales <alexandermorales.n1@gmail.com>
2021-05-06 13:30:06 -0500

    add logic for scraping titles and prices from website

--- bin/main.rb
+++ bin/main.rb
@@ -2,8 +2,36 @@ require '../lib/methods'
 require 'nokogiri'
 require 'open-uri'
 
-puts 'Insert the URL you want to scrape'
+system "cls"
+system "clear"
+puts 'Insert the URL you want to scrape: '
 url = gets.chomp
+sleep(1)
+system "cls"
+system "clear"
+puts 'Insert the name of the container class holding the elements you are interested in: '
+container = gets.chomp
+system "cls"
+system "clear"
+puts 'Insert the name of the title class: '
+titles = gets.chomp
+system "cls"
+system "clear"
+puts 'Insert the name of the prices class: '
+prices = gets.chomp
 
-Url.to_search(url)
-p Url.url
+html = Link.new(url)
+html.listings("#{container}")
+html.titles("#{titles}")
+html.prices("#{prices}")
+
+html.all
+
+
+# https://www.encuentra24.com/panama-es/searchresult/bienes-raices-venta-de-propiedades?regionslug=prov-panama&q=f_price.-false
+
+# articles container = 'container'
+
+# titles class = 'ann-box-title'
+
+# prcices class = 'ann-price-2nd'
