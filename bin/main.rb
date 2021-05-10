13619d5c4bb69ae47f2a215c477768052d8d31c8
Alexander Morales <alexandermorales.n1@gmail.com>
2021-05-10 17:47:14 -0500

    add different example so the tool is easier to use

--- bin/main.rb
+++ bin/main.rb
@@ -1,41 +1,10 @@
 require '../lib/methods'
 require 'nokogiri'
 require 'open-uri'
-require 'csv'
 
-system 'cls'
-system 'clear'
-puts 'Insert the URL you want to scrape: '
-url = gets.chomp
-system 'cls'
-system 'clear'
-puts 'Insert the name of the container class holding the elements you are interested in: '
-container = gets.chomp
-system 'cls'
-system 'clear'
-puts 'Insert the name of the title class: '
-titles = gets.chomp
-system 'cls'
-system 'clear'
-puts 'Insert the name of the prices class: '
-prices = gets.chomp
+doc = Nokogiri::HTML(URI.open("https://www.airbnb.com/s/Bajo-Boquete/homes?place_id=ChIJWYPOC9_spY8RdI653z0wmMA&refinement_paths%5B%5D=%2Fhomes&search_type=section_navigation"))
 
-html = Link.new(url)
-html.listings(container.to_s)
-html.titles(titles.to_s)
-html.prices(prices.to_s)
+articles = doc.css('._8s3ctt')
 
-html.all
-puts html.arr
-
-#
-# Test case
-#
-# URL = https://www.encuentra24.com/panama-es/searchresult/bienes-raices-venta-de-propiedades?regionslug=prov-panama&q=f_price.-false|number.50
-#
-# Container Class = article
-#
-# Title Class = ann-box-title
-#
-# Prices Class = ann-price-2nd
-#
+titles = articles.each { |x| puts x.css('._bzh5lkq').text }
+prices = articles.each { |x| puts x.css('._olc9rf0').text }
