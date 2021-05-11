4be105e9e1ff84d8c8d85b7b35ebd40b8a838f7f
Alexander Morales <alexandermorales.n1@gmail.com>
2021-05-10 19:02:44 -0500

    fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -6,22 +6,17 @@ listings_class = '_8s3ctt'
 title_class = '_bzh5lkq'
 price_class = '_olc9rf0'
 
-# test case 1 
-
+# test case 1
 url1 = 'https://www.airbnb.com/s/Bajo-Boquete/homes?place_id=ChIJWYPOC9_spY8RdI653z0wmMA&refinement_paths%5B%5D=%2Fhomes&search_type=section_navigation'
 
 doc1 = Nokogiri::HTML(URI.open(url1))
 listings = doc1.css(".#{listings_class}")
-titles = listings.each { |x| puts x.css(".#{title_class}").text }
-prices = listings.each { |x| puts x.css(".#{price_class}").text }
+p(listings.map { |x| x.css(".#{title_class}").text })
+p(listings.map { |x| x.css(".#{price_class}").text })
 
-
-# test case 2 
+# test case 2
 url2 = 'https://www.airbnb.com/s/Santa-Clara/homes?place_id=ChIJSbtePuWfrI8RWyPxIedxXic&refinement_paths%5B%5D=%2Fhomes&search_type=section_navigation'
 
-test_2 = Link.new(url2, listings_class, title_class, price_class)
-listings = test_2.listings
-puts test_2.titles
-puts test_2.prices
-
-puts test_2.all
+test2 = Link.new(url2, listings_class, title_class, price_class)
+puts "These are the property titles: #{test2.titles}"
+puts "These are the property prices: #{test2.prices}"
