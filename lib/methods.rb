14ea8a18bb82ea96b34efd3d7e7e21d9a125f9b2
Alexander Morales <alexandermorales.n1@gmail.com>
2021-05-10 19:23:28 -0500

    crete new methods to simplify the use of the tool

--- lib/methods.rb
+++ lib/methods.rb
@@ -1,12 +1,18 @@
 class Link
-  attr_reader :addr, :listings, :titles, :prices, :average, :arr
+  attr_reader :addr, :listings
 
   def initialize(url, attr, title_class, price_class)
     require 'nokogiri'
     require 'open-uri'
     @addr = Nokogiri::HTML(URI.open(url.to_s))
     @listings = @addr.css(".#{attr}".to_s)
+  end
+
+  def titles(title_class)
     @titles = @listings.map { |x| x.css(".#{title_class}").text }
+  end
+
+  def price(price_class)
     @prices = @listings.map { |x| x.css(".#{price_class}").text }
   end
 end
